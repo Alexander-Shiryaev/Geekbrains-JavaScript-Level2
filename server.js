a8d30c1e9d58a72ed7426c46957f09ad61f09f1d
shiryaev585 <69469715+shiryaev585@users.noreply.github.com>
2020-11-12 12:41:33 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    const body = req.url === '/style.css'
-    ? fs.readFileSync('./public/style.css')
-    : fs.readFileSync('./public/index.html');
+
+    let body;
+
+    if (req.url === '/style.css') {
+        body = fs.readFileSync('./public/style.css');
+    } else if (req.url === '/script.js') {
+        body = fs.readFileSync('./public/script.js');
+    } else {
+        body = fs.readFileSync('./public/index.html');
+    }
+
     res.end(body);
 });
 
-server.listen(3000);
+const port = process.env.PORT || 3000;
+server.listen(port);
 
-console.log('Server started!');
+console.log(`Server started on port ${port}`);
