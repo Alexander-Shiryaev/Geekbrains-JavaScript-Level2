0e5545fcdce71b902e350ccc8925f1c594991797
shiryaev585 <69469715+shiryaev585@users.noreply.github.com>
2020-11-16 19:58:22 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,15 +4,29 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url);
 
-    let body;
+    //----------Вариант преподавателя
 
-    if (req.url === '/style.css') {
-        body = fs.readFileSync('./public/style.css');
-    } else if (req.url === '/script.js') {
-        body = fs.readFileSync('./public/script.js');
-    } else {
-        body = fs.readFileSync('./public/index.html');
+    const publicPath = './public';
+
+    let body = null;
+    try {
+        body = fs.readFileSync(`${publicPath}${req.url}`)
+    } catch (e) {
+        console.log(e);
+        body = fs.readFileSync(`${publicPath}/index.html`)
     }
+
+    //----------Мой вариант
+
+    // let body;
+
+    // if (req.url === '/style.css') {
+    //     body = fs.readFileSync('./public/style.css');
+    // } else if (req.url === '/script.js') {
+    //     body = fs.readFileSync('./public/script.js');
+    // } else {
+    //     body = fs.readFileSync('./public/index.html');
+    // }
 
     res.end(body);
 });
