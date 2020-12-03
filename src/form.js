6984dd608d0fa5b3e3f4d15434600c7307472985
shiryaev585 <69469715+shiryaev585@users.noreply.github.com>
2020-12-03 10:54:04 +0300

    Add files via upload

--- src/form.js
+++ src/form.js
@@ -51,7 +51,7 @@ class Form {
             let value = this.value;
             let check;
 
-            if (check = /^\+?(\d{1,3})?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/i.test(value)) {
+            if (check = /^\+?(\d{1,3})?[-]?\(?(?:\d{2,3})\)?[-]?(\d{3})[-]?(\d{4})$/i.test(value)) {
                 this.classList.remove('invalid');
                 this.classList.add('valid');
             } else {
@@ -67,7 +67,7 @@ class Form {
             let value = this.value;
             let check;
 
-            if (check = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i.test(value)) {
+            if (check = /^[A-Z0-9]+[A-Z0-9_%+-\.]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i.test(value)) {
                 this.classList.remove('invalid');
                 this.classList.add('valid');
             } else {
