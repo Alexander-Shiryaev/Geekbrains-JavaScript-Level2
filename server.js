c3eeaa42ed31ebdc9c26f786636a5c09fb96cad5
shiryaev585 <alex_sh_v@mail.ru>
2021-06-24 11:40:35 +0300

    update

--- server.js
+++ server.js
@@ -4,34 +4,20 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url);
 
-    //----------Вариант преподавателя
-
     const publicPath = './public';
 
     let body = null;
     try {
-        body = fs.readFileSync(`${publicPath}${req.url}`)
+        body = fs.readFileSync(`${publicPath}${req.url}`);
     } catch (e) {
         console.log(e);
-        body = fs.readFileSync(`${publicPath}/index.html`)
+        body = fs.readFileSync(`${publicPath}/index.html`);
     }
-
-    //----------Мой вариант
-
-    // let body;
-
-    // if (req.url === '/style.css') {
-    //     body = fs.readFileSync('./public/style.css');
-    // } else if (req.url === '/script.js') {
-    //     body = fs.readFileSync('./public/script.js');
-    // } else {
-    //     body = fs.readFileSync('./public/index.html');
-    // }
 
     res.end(body);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 server.listen(port);
 
 console.log(`Server started on port ${port}`);
